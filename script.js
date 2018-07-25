0e125279290bd3fbcbf2c65fb8881de44f314859
Hajni83 <maro.hajnalka@gmail.com>
2018-07-25 22:40:52 +0200

    fine3

--- script.js
+++ script.js
@@ -8,34 +8,21 @@ $(function(){
         ctx.translate(x,y);
         
         for (var i = 5; i--;) {
-           
-            // draw line up
             ctx.lineTo(0, a);
-            // move origin to current same location as pen
             ctx.translate(0, a);
-            // rotate the drawing board
             ctx.rotate((Math.PI * 2 / 10));
-            // draw line down
             ctx.lineTo(0, -a);
-            // again, move origin to pen...
             ctx.translate(0, -a);
-            // ...and rotate, ready for next arm
             ctx.rotate(-(Math.PI * 6 / 10));
             
         }
-        
-       
         ctx.lineTo(0, a);
         ctx.closePath();
-        
-       
     }
-    
-    filledStar(40,30,15);
-filledStar(100,70,20);
-filledStar(150,100,55);
-ctx.stroke();
-    
-   
-   
+    filledStar(60,40,20);
+    filledStar(160,50,30);
+    filledStar(200,45,40);
+    ctx.fillStyle='#ea9fb9';
+    ctx.fill();
+  
 });
